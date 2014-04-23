9a337f6128b6225436679d531d39b3ace99f5141
Buck Golemon <workitharder@gmail.com>
2014-04-23 11:03:18 -0700

    improve doc, test doc

--- setup.py
+++ setup.py
@@ -1,21 +1,27 @@
 from setuptools import find_packages
 from setuptools import setup
 
-setup(
-    name="yelp_encodings",
-    url='https://github.com/Yelp/yelp_encodings',
-    version="0.1.3",
 
-    author='Buck Golemon',
-    author_email='buck@yelp.com',
-    description='string encodings invented and maintained by yelp',
+def main():
+    setup(
+        name="yelp_encodings",
+        url='https://github.com/Yelp/yelp_encodings',
+        version="0.1.3",
 
-    platforms='any',
-    classifiers=[
-        'License :: Public Domain',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-    ],
+        author='Buck Golemon',
+        author_email='buck@yelp.com',
+        description='string encodings invented and maintained by yelp',
 
-    packages=find_packages('.', exclude=('tests*',)),
-)
+        platforms='any',
+        classifiers=[
+            'License :: Public Domain',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+        ],
+
+        packages=find_packages('.', exclude=('tests*',)),
+    )
+
+
+if __name__ == '__main__':
+    exit(main())
